a25a8541d4cb14fd54caa174d3f5c5ee5f85f401
bena83 <32550079+bena83@users.noreply.github.com>
2022-05-27 16:06:35 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,28 +1,30 @@
-class Student:
-    # [assignment] Skeleton class. Add your code here
-    def __init__(self,name,age,tracks,score):
-        self.name=str(name)
-        self.age=int(age)
-        self.tracks=tracks
-        self.score=float(score)
-    def change_name(self,new_name):
-        self.change_name=new_name
-        print("the new student name is",new_name)
+# Read text from a file, and count the occurence of words in that text
+# Example:
+# count_words("The cake is done. It is a big cake!") 
+# --> {"cake":2, "big":1, "is":2, "the":1, "a":1, "it":1}
+#import string
+def read_file_content(filename):
+    # [assignment] Add your code here 
+  with open ("C:/Users/Admin/Downloads/Reading-Text-Files/story.txt") as f:
+     file=f.read()
+  return file
+result=read_file_content("C:/Users/Admin\Downloads/Reading-Text-Files/story.txt")
+print(result)
+def count_words(str):
+     result= read_file_content("C:/Users/Admin\Downloads/Reading-Text-Files/story.txt")
+    # [assignment] Add your code here
+      # Create an empty dictionary
+     counts = dict()
+     words = str.split()
 
-    def change_age(self,new_age):
-        self.change_age=new_age
-        print("the new age is",new_age)
+    # Loop through each line of the file
+     for word in words:
+        if word in counts:
+            counts[word] += 1
+        else:
+            counts[word] = 1
+     return counts
 
-    def add_track(self,add_track):
-        self.add_track=add_track
-        print("the new student track is",add_track)
-    
-    def get_score(self):
-        return("the new student score is",self.score)
-Bob = Student(name="Bob", age=26, tracks=["FE","BE"],score=20.90)
 
-# Expected methods
-Bob.change_name("Peter")
-Bob.change_age(34)
-Bob.add_track("UI/UX")
-print(Bob.get_score())
+print( count_words(result))
+    #return {"as": 10, "would": 20}
